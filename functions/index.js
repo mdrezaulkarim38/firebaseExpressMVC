da8651f09143a0c6bf436fef1adf6f50c5c7badd
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2024-03-22 10:01:08 +0600

    update the rules

--- functions/index.js
+++ functions/index.js
@@ -1,2 +1,16 @@
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+
+
+app.use(cors({ origin: true}));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get("/api/name",(req,res)=>{
+    res.send("This is test");
+})
+
+exports.app = onRequest(app);
