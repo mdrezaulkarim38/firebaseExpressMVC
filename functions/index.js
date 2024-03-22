e61f18c66a9dfaaa91d749d3d18a2cb3d5aae14c
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2024-03-22 10:19:46 +0600

    db connection is now ok

--- functions/index.js
+++ functions/index.js
@@ -3,14 +3,16 @@ const logger = require("firebase-functions/logger");
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const dbConfig = require("./config/dbConfig");
 const app = express();
+dbConfig.connect();
 
 
 app.use(cors({ origin: true}));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.get("/api/name",(req,res)=>{
-    res.send("This is test");
-})
+app.all("*",(req,res)=>{
+    res.status(404).json({"status": "error", "msg": "404 Not Found"});
+});
 
 exports.app = onRequest(app);
